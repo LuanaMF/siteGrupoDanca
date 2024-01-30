88e353b11356db9646eef8e42c919c04215e2e2a
Luana Fraga <luana@brasilsoftware.com>
2024-01-30 16:54:27 -0300

    div set - tela sobre nos

--- app/page.tsx
+++ app/page.tsx
@@ -12,77 +12,92 @@ import SchoolIcon from '@mui/icons-material/School';
 import StarIcon from '@mui/icons-material/Star';
 import WhatshotIcon from '@mui/icons-material/Whatshot';
 import PlaceIcon from '@mui/icons-material/Place';
-import { LogoMarrom} from "@/components/icons";
 
 function titleQuemSomos(){
   return(
-    <div className=" h-full laranjao">
-      <h1 className={`${title()} row-span-1 justify-self-start sm:text-6xl md:text-5xl lg:text-8xl`}>Quem Somos</h1>
+    <div className=" h-full marrom">
+      <h1 className={`${title()} tracking-wide font-serif row-span-1 justify-self-start sm:text-6xl md:text-5xl lg:text-8xl`}>Quem somos</h1>
     </div>    
   );
 }
 
-function quemSomos() {
-  return (
-    <div className="flex mx-4 my-0 justify-evenly">
-      <Card className="py-4 m-0 rounded-lg border-8 border-pessego border-double bg-transparent w-full lg:w-[500px] sm:w-[350px] h-auto lg:h-[300px] sm:h-[250px] shadow-lg shadow-marrom ">
+function cardSobreNos(){
+  return(
+    <Card 
+      
+      className="
+      levitating m-0 rounded-lg 
+      border-8 border-pessego border-double
+      h-auto 
+      shadow-lg shadow-marrom" >
+        
         <CardHeader className="center">
+
           <Code className="center bg-laranjao w-full opacity-85">
-            <WhatshotIcon sx={{ fontSize: '35px', color: '#601802', marginRight: '2px' }} />
-              <h2 className="icon_label !text-2xl text-center">
+            <WhatshotIcon sx={{ fontSize: '25px', color: '#601802', marginRight: '2px' }} />
+              <h2 className="icon_label !text-xl !text-center">
                   Sobre nós
               </h2> 
-            <WhatshotIcon sx={{ fontSize: '35px', color: '#601802', marginLeft: '2px' }} />
+            <WhatshotIcon sx={{ fontSize: '25px', color: '#601802', marginLeft: '2px' }} />
           </Code>
           
         </CardHeader>
-        <CardBody className="grid grid-rows-3 grid-cols-1 gap-4 ">
-          <h2 className="icon_label center -ml-6">
-            <InfoIcon sx={{  fontSize: '50px' }}></InfoIcon>
-            Somos uma cia de dança
+        <CardBody className=" grid grid-rows-3 grid-cols-1 gap-3 ">
+          <h2 className="icon_label center ml-2">
+            <InfoIcon sx={{  fontSize: '25px' }}></InfoIcon>
+            Aulas | Eventos | Espetáculos
           </h2>
           <h2 className="icon_label center">
-            <ApartmentIcon sx={{  fontSize: '50px' }}></ApartmentIcon>
+            <ApartmentIcon sx={{  fontSize: '25px' }}></ApartmentIcon>
             Empresa de pequeno porte
           </h2>
           <h2 className="icon_label center ">
-            <GroupsIcon sx={{ fontSize: '50px' }}></GroupsIcon>
+            <GroupsIcon sx={{ fontSize: '25px' }}></GroupsIcon>
             25 integrantes (elenco fixo)
           </h2>
         </CardBody>
       </Card>
+  );
+}
 
-      <Card className=" ml-0 rounded-lg border-8 border-pessego border-double bg-transparent w-full lg:w-[500px] sm:w-[400px] lg:h-[350px] sm:h-[350px] shadow-lg shadow-marromVermelho">
+function cardDirecao(){
+  return(
+    <Card 
+        className="
+        levitating
+        ml-0 rounded-lg 
+        border-8 border-pessego border-double 
+        shadow-lg shadow-marromVermelho">
         <CardHeader className="center">
           <Code className="center bg-laranjao w-full opacity-85">
-            <WhatshotIcon sx={{ fontSize: '35px', color: '#601802', marginRight: '2px' }} />
+            <WhatshotIcon sx={{ fontSize: '25px', color: '#601802', marginRight: '2px' }} />
               <h2 className="icon_label !text-2xl text-center">
                 Direção
               </h2>
-            <WhatshotIcon sx={{ fontSize: '35px', color: '#601802', marginLeft: '2px' }} />
+            <WhatshotIcon sx={{ fontSize: '25px', color: '#601802', marginLeft: '2px' }} />
           </Code>
         </CardHeader>
 
-        <CardBody className=" grid grid-rows-4 grid-cols-1 ">
+        <CardBody className=" grid grid-rows-4 grid-cols-1 items-center ">
 
-          <div className="center mb-2">
-            <Avatar radius="full" size="lg" src="/imgs/fotoPessoa.png" />
+          <div className="center">
+            <Avatar radius="full" size="md" src="/imgs/fotoPessoa.png" />
             <div className="flex flex-col items-start justify-center ">
-              <h4 className="text-lg font-bold leading-none text-marrom ml-2">Juliana Mascarenhas</h4>
+              <h4 className="text-md font-bold leading-none text-marrom ml-2">Juliana Mascarenhas</h4>
             </div>
           </div>
 
-          <h2 className="icon_label items-center -mb-5">
-            <InfoIcon sx={{ fontSize: '40px', color: "#601802" }}></InfoIcon>
+          <h2 className="icon_label">
+            <InfoIcon sx={{ fontSize: '25px', color: "#601802" }}></InfoIcon>
             Uma das fundadoras iniciais
           </h2>
-          <h2 className="icon_label items-center">
-            <StarIcon sx={{ fontSize: '40px', color: "#601802" }}></StarIcon>
+          <h2 className="icon_label ">
+            <StarIcon sx={{ fontSize: '25px', color: "#601802" }}></StarIcon>
             Dançarina e coreógrafa
           </h2>
           <div>
-            <h2 className="icon_label items-center">
-              <SchoolIcon sx={{ fontSize: '40px', color: "#601802" }}></SchoolIcon>
+            <h2 className="icon_label">
+              <SchoolIcon sx={{ fontSize: '25px', color: "#601802" }}></SchoolIcon>
                 Licenciada e Bacharel em Dança 
                  -
                 <span className="text-marrom center font-bold items-center -ml-1">
@@ -95,13 +110,83 @@ function quemSomos() {
           
         </CardBody>
       </Card>
+  );
+}
+
+function cardHistoria(){
+  return(
+    <Card className=" bg-pessego levitating w-full h-full">
+
+    </Card>
+  );
+}
+
+function cardMissao(){
+  return(
+    <Card className="bg-pessego levitating w-full h-full">
+
+    </Card>
+  );
+}
+
+function cardValores(){
+  return(
+    <Card className="bg-pessego levitating w-full h-full">
+
+    </Card>
+  );
+}
+
+function cardVisao(){
+  return(
+    <Card className="bg-pessego levitating">
+
+    </Card>
+  );
+}
+
+function quemSomos() {
+  return (
+    <div className="grid grid-rows-3 grid-cols-4 gap-6" style={{ gridTemplateColumns: '2fr 2fr 1fr 1fr', gridTemplateRows: '1fr 2fr 1fr' }}>
+
+      
+      <div className=" col-span-2">
+        {titleQuemSomos()}
+      </div>
+
+      
+      <div className="mt-8 row-span-3 col-span-2">
+        {cardHistoria()}
+      </div>
+
+      
+      <div className="-mt-2 w-[80%]">
+        {cardSobreNos()}
+      </div>
+
+      
+      <div className="-ml-10 -mt-4">
+        {cardDirecao()}
+      </div>
+
+      
+      <div className="-mt-4 w-[90%]">
+        {cardValores()}
+      </div>
+
+      
+      <div className="">
+        {cardMissao()}
+      </div>
+
     </div>
+
   );
 }
 
 function Principal(){
   return(
-    <div className="grid grid-rows-3 grid-cols-3 ">
+    <div className="grid grid-rows-3 grid-cols-3">
 
       <div className="w-full h-full flex justify-center row-span-2">
         <Image
@@ -115,7 +200,7 @@ function Principal(){
      </div>
 
      <div className="flex items-center justify-self-center row-start-1  marrom">
-        <h1 className={`${title()} font-serif row-span-1 sm:text-6xl md:text-5xl lg:text-8xl`}>A CIA</h1>
+        <h1 className={`${title()} tracking-widest font-serif row-span-1 sm:text-6xl md:text-5xl lg:text-8xl`}>A CIA</h1>
       </div>
 
      <div className="w-full h-full flex justify-center">
@@ -155,21 +240,8 @@ function Principal(){
   );
 }
 
-const slidesData = [
-  {
-    content: Principal(),
-  },
-  {
-    title: titleQuemSomos(),
-    content: quemSomos(),
-  },
-  {
-    title: "Nossa História",
-    content: "A InFlame foi fundada em outubro de 2012 em Feira de Santana, Bahia, a partir de uma apresentação realizada no Colégio Padre Ovídio pelas alunas do 1º ano. A princípio, definiu-se como um grupo de dança o qual participava de eventos culturais da cidade. Em 2018, estabeleceu-se como companhia e tem como foco a realização de eventos, espetáculos, oficinas e aulas regulares de dança.",
-  },
-];
-
 export default function SobreNos() {
+
   const sliderSettings = {
     dots: true,
     infinite: true,
@@ -192,23 +264,14 @@ export default function SobreNos() {
     <section id="section-sobreNos" className="body-with-grain-texture bg-cover mix-blend-multiply flex flex-col items-center justify-center h-screen overflow-hidden">
       <div className="justify-center w-[90%] h-[80%]">
         <Slider {...sliderSettings} className="grid w-full h-full border-none">
-          {slidesData.map((slide, index) => (
-            index !== 0 ? (
-              <div key={index} id="grid" className="grid grid-rows-3">
-                <div className="w-full h-full row-span-1">
-                  {slide.title}
-                </div>
-                <div className="-mt-2 w-full h-full row-span-2">
-                  {slide.content}
-                </div>
-              </div>
-            ) : (
-              <div key={index}>
-                {slide.content}
-              </div>
-            )
-            
-          ))}
+        
+          <div key={1}>
+            {Principal()}
+          </div>
+          <div key={2}>
+            {quemSomos()}
+          </div>
+           
         </Slider>
       </div>
     </section>
